920c2d72201917fffd8083986382d70c863da685
Andy Grove <andygrove73@gmail.com>
2018-02-18 22:06:15 -0700

    correct class name in script

--- bin/bench-scala-generate-wkt.sh
+++ bin/bench-scala-generate-wkt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sbt assembly
-java -Xms1g -Xmx2g -classpath ./target/scala-2.11/datafusion-benchmarks-assembly-0.1.0.jar GenerateWkt
+java -Xms1g -Xmx2g -classpath ./target/scala-2.11/datafusion-benchmarks-assembly-0.1.0.jar GenerateWKT
