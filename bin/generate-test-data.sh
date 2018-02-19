fc2d9f8ffe05233783230dd517fee40351429e1b
Andy Grove <andygrove73@gmail.com>
2018-02-18 17:07:11 -0700

    fix paths in bash files

--- bin/generate-test-data.sh
+++ bin/generate-test-data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sbt assembly
-java -Xms1g -Xmx2g -classpath /home/andy/git/datafusion-benchmarks/target/scala-2.11/bigdata-benchmarks-assembly-0.1.0.jar DataGenerator
+java -Xms1g -Xmx2g -classpath ./target/scala-2.11/bigdata-benchmarks-assembly-0.1.0.jar DataGenerator
